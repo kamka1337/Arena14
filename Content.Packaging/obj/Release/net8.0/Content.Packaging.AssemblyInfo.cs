f68efb066afdfdd4aa6b26e7ca431f26f7ae7b9b
sandmit <170251214+sandmit@users.noreply.github.com>
2026-05-25 14:02:43 +0300

    1

--- Content.Packaging/obj/Release/net8.0/Content.Packaging.AssemblyInfo.cs
+++ Content.Packaging/obj/Release/net8.0/Content.Packaging.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Content.Packaging")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c728e2a6f08a6dd2c3799b94363008aa8a876515")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5418ed2cb720477f793ebf5ef2c10839ca4f397")]
 [assembly: System.Reflection.AssemblyProductAttribute("Content.Packaging")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Content.Packaging")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
