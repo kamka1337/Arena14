8981fec29b4a62851b9e342a022b9c926d77897f
sandmit <170251214+sandmit@users.noreply.github.com>
2026-05-25 13:41:38 +0300

    1

--- Content.Benchmarks/obj/Release/Content.Benchmarks.AssemblyInfo.cs
+++ Content.Benchmarks/obj/Release/Content.Benchmarks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Content.Benchmarks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c728e2a6f08a6dd2c3799b94363008aa8a876515")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e47fa45ed22cc4ab78034df1e8e86b532ffa246e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Content.Benchmarks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Content.Benchmarks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Content.Replay/obj/Release/Content.Replay.AssemblyInfo.cs
+++ Content.Replay/obj/Release/Content.Replay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Content.Replay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c728e2a6f08a6dd2c3799b94363008aa8a876515")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e47fa45ed22cc4ab78034df1e8e86b532ffa246e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Content.Replay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Content.Replay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
