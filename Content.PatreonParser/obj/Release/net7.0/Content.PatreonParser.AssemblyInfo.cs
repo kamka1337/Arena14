06838a7fe94cc6d75bc68a6b01165a2cf466c8e1
sandmit <170251214+sandmit@users.noreply.github.com>
2026-07-22 19:12:58 +0300

    хуйня

--- Content.PatreonParser/obj/Release/net7.0/Content.PatreonParser.AssemblyInfo.cs
+++ Content.PatreonParser/obj/Release/net7.0/Content.PatreonParser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Content.PatreonParser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5418ed2cb720477f793ebf5ef2c10839ca4f397")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e48533cba09d881cf62b04888d5db6457f8beca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Content.PatreonParser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Content.PatreonParser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Pow3r/obj/Release/net8.0/Pow3r.AssemblyInfo.cs
+++ Pow3r/obj/Release/net8.0/Pow3r.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pow3r")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5418ed2cb720477f793ebf5ef2c10839ca4f397")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e48533cba09d881cf62b04888d5db6457f8beca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pow3r")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pow3r")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
