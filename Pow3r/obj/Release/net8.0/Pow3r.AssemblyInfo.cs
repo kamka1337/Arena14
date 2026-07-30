81d2432a2d33f76487f2c0b22ae759e8d07c56b4
sandmit <170251214+sandmit@users.noreply.github.com>
2026-07-30 14:46:27 +0300

    1

--- Pow3r/obj/Release/net8.0/Pow3r.AssemblyInfo.cs
+++ Pow3r/obj/Release/net8.0/Pow3r.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pow3r")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e48533cba09d881cf62b04888d5db6457f8beca1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40901aecad08fae79e792a1586504677ffff610b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pow3r")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pow3r")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
